b77095f4286f54e4095921c995ab82aecb5ae9d1
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-11 12:01:45 +0300

    learning The filter method on arrays

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array,name){
+    return array.filter(function(driver){
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+
+function fuzzyMatch(array,firstLetter){
+return array.filter(function(driver){
+    return driver.toLowerCase().startsWith(firstLetter.toLowerCase())
+})
+}
+
+function matchName(array,hometown){
+    return array.filter(function (driver){
+        return driver.name === hometown
+    })
+}
